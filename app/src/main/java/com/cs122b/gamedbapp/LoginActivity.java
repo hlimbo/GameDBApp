4cc0bba36b026334a1d6b3f5687c8f2488504533
hlimbo <limboh27@gmail.com>
2017-05-28 21:30:05 -0700

    Switch back to HTTP protocol and forward login credentials to xlogin servlet

--- app/src/main/java/com/cs122b/gamedbapp/LoginActivity.java
+++ app/src/main/java/com/cs122b/gamedbapp/LoginActivity.java
@@ -1,7 +1,25 @@
 package com.cs122b.gamedbapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import constants.constants;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +27,54 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    //Used when Login button is tapped
+    public void onLoginClick(View view)
+    {
+        EditText emailText = (EditText) this.findViewById(R.id.email);
+        EditText passwordText = (EditText) this.findViewById(R.id.password);
+
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+
+        String base_url = "http://" + constants.IP + ":" + constants.HTTP_PORT;
+        String path = "/servlet/xlogin";
+        String full_url = base_url + path + "?email=" + email + "&password=" + password;
+
+        final Context context = this;
+        Intent intent = new Intent(context, SuccessfulLoginActivity.class);
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        Log.d("INFO",full_url);
+
+        final Map<String, String> params = new HashMap<String,String>();
+        //TODO(HARVEY): Use POST instead of GET later on
+        StringRequest postRequest = new StringRequest(Request.Method.GET, full_url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        Log.d("RESPONSE",response);
+                        TextView textView = (TextView) findViewById(R.id.textView);
+                        textView.setText(response.toString());
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        Log.d("SECURITY.ERROR",error.toString());
+                    }
+                }
+                ) {
+                    @Override
+                    protected Map<String, String> getParams() { return params; }
+                };
+
+        queue.add(postRequest);
     }
 }
---
+++ app/src/main/java/constants/constants.java
@@ -0,0 +1,14 @@
+package constants;
+
+/**
+ * Created by Harvey on 5/28/2017.
+ */
+
+public final class constants
+{
+    //aws-IP website
+    public static final String IP = "34.209.119.249";
+    public static final String HTTPS_PORT = "8443";
+    public static final String HTTP_PORT = "8080";
+
+}
